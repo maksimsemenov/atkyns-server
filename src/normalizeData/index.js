6eedc4c0030411d7421fe575852c0982a8427333
maksimsemenov <maksa.semenov@gmail.com>
2016-10-12 23:30:08 -0400

    Add form normalisation
    Add base for data normalisation files

---
+++ src/normalizeData/index.js
@@ -0,0 +1,5 @@
+const normI485 = require('./normI485')
+
+module.exports = {
+  'i-485': normI485
+}
---
+++ src/normalizeData/normI485.js
@@ -0,0 +1,54 @@
+const normI485 = (data) => {
+  return {
+    name: {
+      family: 'Shulbert',
+      first: 'Albert',
+      middle: '',
+      i94: 'Albert Shulbert'
+    },
+    fatherName: 'Uiri',
+    motherName: 'Olga',
+    gender: 'female',
+    maritalStatus: 'widowed',
+    address: {
+      street: '31237921 ksdl lk kldas',
+      apt: '1275',
+      co: 'Albert Shulbert',
+      city: 'Brooykln',
+      state: 'New York',
+      zip: 12123
+    },
+    birth: {
+      date: '11/02/1986',
+      country: 'Russia',
+      city: 'fdkfldfhjs'
+    },
+    citizenship: 'Russia',
+    ssn: '43424242',
+    aNumber: '342421423',
+    entry: {
+      date: '03/01/2015',
+      i94: '432-43424253',
+      status: 'Visitor',
+      place: 'New York, NY',
+      inspected: true,
+      visa: {
+        number: '3218930123810',
+        consulate: 'Ekaterinburg',
+        issueDate: '12/21/2014'
+      }
+    },
+    currentStatus: {
+      status: 'F-1',
+      expires: '12/21/2020'
+    },
+    aplicationStatus: 'b',
+    occupation: 'Designer',
+    priorApplication: '03/20/2010, New-York, US, declined',
+    family: [
+
+    ]
+  }
+}
+
+module.exports = normI485
